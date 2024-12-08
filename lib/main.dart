82158bf17d9599fb867460f33b98ac721cba4b93
Maksim Nazaruk <maksim.nazaruk@gmail.com>
2024-12-08 21:20:24 +0100

    basic list layout: 1

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,49 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Star Wars',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow,
         ),
       ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text('Star Wars'),
+      ),
+      body: ListView(
+        children: const [
+          CharacterTile('Luke Skywalker'),
+          CharacterTile('Darth Vader'),
+        ],
+      ),
+    );
+  }
+}
+
+class CharacterTile extends StatelessWidget {
+  const CharacterTile(this.name);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(name),
     );
   }
 }
